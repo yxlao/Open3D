92654088b2dfe7771b028a697a72822d9a8bd2a7
Yixing Lao <yixing.lao@gmail.com>
2020-01-16 17:39:43 -0800

    add torch test

--- src/UnitTest/Python/test_core.py
+++ src/UnitTest/Python/test_core.py
@@ -198,6 +198,21 @@ def test_tensor_to_numpy_scope():
     print(dst_t.base)
 
 
+def test_tensor_to_numpy_scope_pytorch():
+    src_t = np.array([[10, 11, 12.], [13., 14., 15.]])
+
+    def get_dst_t():
+        o3d_t = torch.Tensor(src_t) # Copy
+        dst_t = o3d_t.numpy()
+        print(dst_t)
+        return dst_t
+
+    dst_t = get_dst_t()
+    np.testing.assert_equal(dst_t, src_t)
+    print(dst_t)
+    print(dst_t.base)
+
+
 @pytest.mark.parametrize("device", list_devices())
 def test_tensor_from_pytorch(device):
     device_id = device.get_id()
