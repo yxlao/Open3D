bbc50c34fe22e7bfc87ea4772d42db0544e727b9
Yixing Lao <yixing.lao@gmail.com>
2020-02-13 03:47:27 -0800

    n=32 for now

--- src/UnitTest/Core/Tensor.cpp
+++ src/UnitTest/Core/Tensor.cpp
@@ -1023,26 +1023,27 @@ TEST_P(TensorPermuteDevices, SumCUDA) {
         return;
     }
 
-    Tensor src(std::vector<float>({0,  1,  2,  3,  4,  5,  6,  7,
-                                   8,  9,  10, 11, 12, 13, 14, 15,
-                                   16, 17, 18, 19, 20, 21, 22, 23}),
-               {2, 3, 4}, Dtype::Float32, device);
+    Tensor src(std::vector<float>({0,  1,  2,  3,  4,  5,  6,  7,  8,  9,  10,
+                                   11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+                                   22, 23, 24, 25, 26, 27, 28, 29, 30, 31}),
+               {2, 4, 4}, Dtype::Float32, device);
     Tensor dst;
 
     dst = src.Sum({0, 1, 2}, true);
     EXPECT_EQ(dst.GetShape(), SizeVector({1, 1, 1}));
-    EXPECT_EQ(dst.ToFlatVector<float>(), std::vector<float>({276}));
+    EXPECT_EQ(dst.ToFlatVector<float>(), std::vector<float>({496}));
 }
 
 TEST_P(TensorPermuteDevices, ProdSumCUDA) {
     Device device = GetParam();
 
-    Tensor src(std::vector<float>({1, 1, 2, 3, 3, 2, 1, 1, 1, 1, 2, 3,
-                                   3, 2, 1, 1, 1, 1, 2, 3, 3, 2, 1, 1}),
-               {2, 3, 4}, Dtype::Float32, device);
+    Tensor src(std::vector<float>({1, 1, 2, 3, 3, 2, 1, 1, 1, 1, 2,
+                                   3, 3, 2, 1, 1, 1, 1, 2, 3, 3, 2,
+                                   1, 1, 3, 2, 1, 1, 1, 1, 2, 3}),
+               {2, 4, 4}, Dtype::Float32, device);
     Tensor dst;
 
     dst = src.Prod({0, 1, 2}, true);
     EXPECT_EQ(dst.GetShape(), SizeVector({1, 1, 1}));
-    EXPECT_EQ(dst.ToFlatVector<float>(), std::vector<float>({46656}));
+    EXPECT_EQ(dst.ToFlatVector<float>(), std::vector<float>({1679616}));
 }
