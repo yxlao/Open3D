6d591a920ea09e9384aec2a976a66d4ad0b6cc13
Yixing Lao <yixing.lao@gmail.com>
2020-02-13 22:13:19 -0800

    cuda recursive reduction

--- src/Open3D/Core/Kernel/CUDALauncherReduction.cuh
+++ src/Open3D/Core/Kernel/CUDALauncherReduction.cuh
@@ -177,6 +177,77 @@ __global__ void ReduceKernelInit(Indexer indexer,
 }
 
 template <typename scalar_t, typename func_t>
+__global__ void ReduceKernelBlock(scalar_t identity,
+                                  func_t element_kernel,
+                                  scalar_t* g_idata,
+                                  scalar_t* g_odata,
+                                  unsigned int n) {
+    cg::thread_block cta = cg::this_thread_block();
+    scalar_t* sdata = SharedMemory<scalar_t>();
+    unsigned int tid = threadIdx.x;
+    unsigned int i = blockIdx.x * blockDim.x * 2 + threadIdx.x;
+    unsigned int grid_stride = blockDim.x * 2 * gridDim.x;
+
+    // Reduce multiple elements per thread. Larger gridDim.x results in larger
+    // grid_stride and fewer elements per thread.
+    scalar_t local_result = identity;
+    while (i < n) {
+        // local_result += g_idata[i];
+        element_kernel(&g_idata[i], &local_result);
+        if (i + blockDim.x < n) {
+            // local_result += g_idata[i + blockDim.x];
+            element_kernel(&g_idata[i + blockDim.x], &local_result);
+        }
+        i += grid_stride;
+    }
+    sdata[tid] = local_result;
+    cg::sync(cta);
+
+    // Unrolled: 512, 256, 128.
+    if (blockDim.x >= 512 && tid < 256) {
+        // local_result += sdata[tid + 256];
+        element_kernel(&sdata[tid + 256], &local_result);
+        sdata[tid] = local_result;
+    }
+    cg::sync(cta);
+    if (blockDim.x >= 256 && tid < 128) {
+        // local_result += sdata[tid + 128];
+        element_kernel(&sdata[tid + 128], &local_result);
+        sdata[tid] = local_result;
+    }
+    cg::sync(cta);
+    if (blockDim.x >= 128 && tid < 64) {
+        // local_result += sdata[tid + 64];
+        element_kernel(&sdata[tid + 64], &local_result);
+        sdata[tid] = local_result;
+    }
+    cg::sync(cta);
+
+    // Single warp reduction with shuffle: 64, 32, 16, 8, 4, 2, 1
+    cg::thread_block_tile<32> tile32 = cg::tiled_partition<32>(cta);
+    scalar_t local_temp = identity;
+    if (cta.thread_rank() < 32) {
+        // Fetch final intermediate result from 2nd warp
+        if (blockDim.x >= 64) {
+            // local_result += sdata[tid + 32];
+            element_kernel(&sdata[tid + 32], &local_result);
+        }
+        // Reduce final warp using shuffle
+        for (int offset = tile32.size() / 2; offset > 0; offset /= 2) {
+            if (blockDim.x >= offset * 2) {
+                local_temp = tile32.shfl_down(local_result, offset);
+                element_kernel(&local_temp, &local_result);
+            }
+        }
+    }
+
+    // Write result for this block to global mem.
+    if (cta.thread_rank() == 0) {
+        g_odata[blockIdx.x] = local_result;
+    }
+}
+
+template <typename scalar_t, typename func_t>
 void LaunchReductionKernelOneOutput(const Indexer& indexer,
                                     scalar_t identity,
                                     func_t element_kernel) {
@@ -198,12 +269,35 @@ void LaunchReductionKernelOneOutput(const Indexer& indexer,
     OPEN3D_CUDA_CHECK(
             cudaMalloc((void**)&d_tdata, grid_size * sizeof(scalar_t)));
 
+    // First pass reduction, read from Tensor.
     ReduceKernelInit<scalar_t><<<grid_size, block_size,
                                  GetSMSize<scalar_t>(grid_size, block_size)>>>(
             indexer, identity, element_kernel, d_odata, n);
+    OPEN3D_GET_LAST_CUDA_ERROR("Kernel execution failed.");
+
+    // Reduce the partial results from blocks. No need to read from Tensor.
+    n = grid_size;
+    while (n > 1) {
+        std::tie(grid_size, block_size) = GetGridSizeBlockSize(n);
+        utility::LogInfo("n={}, grid_size={}, block_size={}", n, grid_size,
+                         block_size);
+        // Input: d_tdata, output: d_odata
+        OPEN3D_CUDA_CHECK(cudaMemcpy(d_tdata, d_odata, n * sizeof(scalar_t),
+                                     cudaMemcpyDeviceToDevice));
+        ReduceKernelBlock<scalar_t>
+                <<<grid_size, block_size,
+                   GetSMSize<scalar_t>(grid_size, block_size)>>>(
+                        identity, element_kernel, d_tdata, d_odata, n);
+        OPEN3D_GET_LAST_CUDA_ERROR("Kernel execution failed.");
+        n = (n + (block_size * 2 - 1)) / (block_size * 2);
+    }
 
     OPEN3D_CUDA_CHECK(cudaMemcpy(indexer.GetOutputPtr(0), d_odata,
                                  sizeof(scalar_t), cudaMemcpyDeviceToHost));
+
+    // Clean up
+    OPEN3D_CUDA_CHECK(cudaFree(d_odata));
+    OPEN3D_CUDA_CHECK(cudaFree(d_tdata));
 }
 
 }  // namespace cuda_launcher
