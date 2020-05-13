aaf74b1d057232936dc70104aa89656f106ac263
Yixing Lao <yixing.lao@gmail.com>
2020-05-12 17:22:00 -0700

    add knn partial code

--- src/UnitTest/Core/Device.cpp
+++ src/UnitTest/Core/Device.cpp
@@ -25,7 +25,15 @@
 // ----------------------------------------------------------------------------
 
 #include "Open3D/Core/Device.h"
+#include "Open3D/Core/AdvancedIndexing.h"
+#include "Open3D/Core/Dtype.h"
+#include "Open3D/Core/Kernel/Kernel.h"
+#include "Open3D/Core/MemoryManager.h"
+#include "Open3D/Core/SizeVector.h"
+#include "Open3D/Core/Tensor.h"
+#include "Open3D/Utility/Helper.h"
 
+#include "Core/CoreTest.h"
 #include "TestUtility/UnitTest.h"
 
 using namespace std;
@@ -59,3 +67,47 @@ TEST(Device, StringConstructorLower) {
     EXPECT_EQ(ctx.GetType(), Device::DeviceType::CUDA);
     EXPECT_EQ(ctx.GetID(), 1);
 }
+
+TEST(Device, TensorPtr) {
+    Device device("CPU:0");
+    Tensor a;
+    a = Tensor::Ones({2, 3}, Dtype::Float32, device);
+    a = Tensor(std::vector<float>({0, 1, 2, 3, 4, 5}), {2, 3}, Dtype::Float32,
+               device);
+    a = a.Contiguous();
+
+    EXPECT_EQ(a.GetShape(), SizeVector({2, 3}));
+    EXPECT_EQ(a.NumElements(), 6);
+
+    std::cout << a.ToString() << std::endl;
+    std::cout << a[0].ToString() << std::endl;
+
+    // float* ptr = static_cast<float*>(a.GetDataPtr());
+    // std::cout << ptr << std::endl;
+    // std::cout << ptr[0] << std::endl;
+    // std::cout << ptr[1] << std::endl;
+    // std::cout << ptr[2] << std::endl;
+}
+
+class KNN {
+public:
+    KNN(const Tensor& tensor) {
+        if (tensor.NumDims() != 2) {
+            utility::LogError("must be N * 2");
+        }
+        dim_ = tensor.GetShape()[1];
+    }
+
+    int KNNSearch(const T& query,
+                  int knn,
+                  std::vector<int>& indices,
+                  std::vector<float>& distance2);
+
+    int RadiusSearch(const T& query,
+                     float radius,
+                     std::vector<int>& indices,
+                     std::vector<float>& distance2);
+
+private:
+    int dim_ = 0;
+};
